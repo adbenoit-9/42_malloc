05d40fa29a950c7c1d5aeb206fc4c1e42ab8c669
adbenoit-9 <adeline.benoit@hotmail.com>
2022-08-04 11:25:15 +0200

    [UPDATE] realloc

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 14:12:45 by adbenoit          #+#    #+#             */
-/*   Updated: 2022/08/02 18:57:16 by adbenoit         ###   ########.fr       */
+/*   Updated: 2022/08/04 11:24:06 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 void	*realloc(void *ptr, size_t size)
 {
-    (void)size;
-    return (ptr);
+	void	*new_ptr;
+
+	if (!ptr)
+		return (malloc(size));
+	if (size) {
+		new_ptr = malloc(size);
+		if (!new_ptr)
+			return (NULL);
+	}
+	for (size_t i = 0; ((unsigned char *)ptr)[i] && i < size; i++)
+		((unsigned char *)new_ptr)[i] = ((unsigned char *)ptr)[i];
+	free(ptr);
+	return (new_ptr);
 }
